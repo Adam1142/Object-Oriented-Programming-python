ece70bf80b46196b9657da944bae52fe4d869e9c
Adam1142 <doodoohsb@gmail.com>
2025-08-11 17:27:02 +0100

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,5 @@
+# Write a program to create a class with the name of student and preform the following task 1. declare a variable 2. print a sentence inside the class 3. create an object of class student and see the output
+class student:
+    grade = 10
+    print("Hi I am a student of grade", grade)
+object = student()
